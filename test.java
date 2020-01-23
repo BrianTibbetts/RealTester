c13a8817678f8cf423863962c8b1112207102d53
Brian A Tibbetts <brian.tibbetts@maine.edu>
2020-01-23 12:42:33 -0500

    Wow I hope I didn't make some ridiculous mistake like adding HTML to a Java file

--- test.java
+++ test.java
@@ -3,7 +3,8 @@ public class TestClass {
 	public static void main(String[] args){
 
 		System.out.println("Hello world!");
-		
+		System.out.println("What's shaking?");
+		<html> Hrmmm this seems like good Java to me</html>
 	}
 
 }
